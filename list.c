f42d7114a391d9fd4c965399f96a744ea70ca7d5
Michał Treter <adimus13@gmail.com>
2018-02-28 01:19:57 +0100

    program fixed

--- list.c
+++ list.c
@@ -4,151 +4,189 @@
 #include <sys/time.h>
 
 struct List{
-    int value;
-    struct List *next;
+    int size;
+    struct Node *start;
 };
 
-struct List *newList(int value){
+struct Node{
+    int value;
+    struct Node *next;
+};
+
+struct List *newList(){
     struct List *l = malloc(sizeof(struct List));
-    l->next = NULL;
-    l->value = value;
+    l->start = NULL;
+    l->size = 0;
 
     return l;
 }
 
-void printList(struct List *l){
-    printf("[ ");
-    
-    while(l->next != NULL){
-        printf("%d, ", l->value);
-        l = l->next;
-    }
-
-    printf("%d ]\n", l->value);
-}
-
 void addToList(struct List *l, int value){
+    struct Node *temp = l->start;
 
-    while(l->next != NULL){
-        l = l->next;
+    if(l->size == 0){
+        l->start = malloc(sizeof(struct Node));
+        l->start->value = value;
+        l->start->next = NULL;
+        l->size = 1;
     }
+    else{
 
-    l->next = malloc(sizeof(struct List));
-    l->next->next = NULL;
-    l->next->value = value;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+
+        temp->next = malloc(sizeof(struct List));
+        temp->next->next = NULL;
+        temp->next->value = value;
+        l->size++;
+    }
 }
 
 int getElementAt(struct List *l, int position){
     int iterator = 0;
-
-    while(iterator < position){
-        iterator++;
-        if(l == NULL){
-            printf("Such index do not exists, printing 0...\n");
-            return 0;
-        }
-
-        l = l->next;
-    }
-
-    if(l == NULL){
+    if(position >= l->size){
         printf("Such index do not exists, printing 0...\n");
-        return 0;
+            return 0;
     }
 
-    return l->value;
-}
-
-void deleteElementAt(struct List **l, int position){
-    int iterator = 0;
-    struct List *prev = NULL;
-    struct List *next = NULL;
-    struct List *temp = *l;
+    struct Node *temp = l->start;
 
     while(iterator < position){
         iterator++;
-        if(temp == NULL){
-            printf("Such index do not exists...\n");
-            return ;
-        }
-        prev = temp;
         temp = temp->next;
     }
 
-    if(temp == NULL){
+    return temp->value;
+}
+
+void deleteElementAt(struct List *l, int position){
+    int iterator = 0;
+
+    if(position >= l->size){
         printf("Such index do not exists...\n");
-        return ;
+            return ;
     }
 
-    if(prev == NULL){
-        temp = (*l)->next;
-        free(*l);
-        *l = temp;
-        return ;
-    }
-
-    if(temp->next == NULL){
-        prev->next = NULL;
+    if(position == 0){
+        struct Node *temp = l->start;
+        l->start = temp->next;
+        l->size--;
         free(temp);
         return ;
     }
 
-    prev->next = temp->next;
-    free(temp);
-}
+    struct Node *temp = l->start;
+    struct Node *prev = NULL;
 
-struct List *megre(struct List **a, struct List **b){
-    struct List *newList = *a;
-    struct List *temp = *a;
-
-    while(temp->next != NULL){
+    while(iterator < position){
+        iterator++;
+        prev = temp;
         temp = temp->next;
     }
 
-    temp->next = *b;
+    prev->next = temp->next;
+    l->size--;
+    free(temp);
+}
 
-    *a = NULL;
-    *b = NULL;
+int getElementPosition(struct List *l, int element){
 
-    return newList;
+    if(l->size == 0){
+        printf("List is empty...\n");
+        return -1;
+    }
+
+    struct Node *temp = l->start;
+    int iterator = 0;
+
+    while(temp->value != element){
+        temp = temp->next;
+        iterator++;
+        if(temp == NULL){
+            printf("Such element does not ezists...\n");
+            return -1;
+        }
+    }
+
+    return iterator;
+}
+
+void printList(struct List *l){
+    struct Node *temp = l->start;
+    printf("[ ");
+    
+    while(temp->next != NULL){
+        printf("%d, ", temp->value);
+        temp = temp->next;
+    }
+
+    printf("%d ]\n", temp->value);
+}
+
+struct List *megre(struct List *a, struct List *b){
+    struct List *cList = newList();
+    struct Node *temp = a->start;
+
+    while(temp != NULL){
+        addToList(cList, temp->value);
+        temp = temp->next;
+    }
+
+    temp = b->start;
+
+    while(temp != NULL){
+        addToList(cList, temp->value);
+        temp = temp->next;
+    }
+
+    return cList;
 }
 
 int main(){
     srand(time(NULL));
     
-    struct List *list = newList(10);
+    struct List *list = newList();
     addToList(list, 14);
     addToList(list, 120);
     addToList(list, 130);
     addToList(list, 1);
 
+    printf("Element selection test:\n");
     printList(list);
 
     printf("%d\n", getElementAt(list, 3));
     printf("%d\n", getElementAt(list, 100));
 
-    deleteElementAt(&list, 4);
+    printf("Delete test:\n");
+
+    deleteElementAt(list, 4);
     printList(list);
 
-    deleteElementAt(&list, 2);
+    deleteElementAt(list, 2);
     printList(list);
 
-    deleteElementAt(&list, 0);
+    deleteElementAt(list, 0);
     printList(list);
 
-    struct List *list2 = newList(54);
-    addToList(list, 334);
-    addToList(list, 1204);
-    addToList(list, 1302);
-    addToList(list, 11);
+    struct List *list2 = newList();
+    addToList(list2, 334);
+    addToList(list2, 1204);
+    addToList(list2, 1302);
+    addToList(list2, 11);
 
-    struct List *newL = megre(&list, &list2);
+    printf("Index selection test:\n");
+    printf("%d\n", getElementPosition(list2, 334));
+    printf("%d\n", getElementPosition(list2, 11));
+    printf("%d\n", getElementPosition(list2, 69));
+
+    struct List *newL = megre(list, list2);
 
     printList(newL);
 
-    struct List *bigList = newList(rand()%1001);
+    struct List *bigList = newList();
     int i;
-    for(i = 0; i < 999; i++){
+    for(i = 0; i < 1000; i++){
         addToList(bigList, rand()%1001);
     }
 
@@ -159,7 +197,9 @@ int main(){
     struct timeval  tv1, tv2;
 
     gettimeofday(&tv1, NULL);
-    selected = getElementAt(bigList, 512);
+    for(i = 0; i < 1000; i++){
+        selected = getElementAt(bigList, 512);
+    }
     gettimeofday(&tv2, NULL);
 
     printf ("Selecting the same element lasts %f seconds\n",
@@ -167,7 +207,9 @@ int main(){
             (double) (tv2.tv_sec - tv1.tv_sec));
 
     gettimeofday(&tv1, NULL);
-    selected = getElementAt(bigList, rand()%1000);
+    for(i = 0; i < 1000; i++){
+        selected = getElementAt(bigList, rand()%1000);
+    }
     gettimeofday(&tv2, NULL);
 
     printf ("Selecting random element lasts %f seconds\n",
